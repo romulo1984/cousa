6596b6db7be3ebc0f262b2f54a00622beee7231e
Rômulo Guimarães <romulo1984@gmail.com>
2018-06-27 00:24:33 -0300

    New change

--- pages/book/_slug.vue
+++ pages/book/_slug.vue
@@ -7,13 +7,13 @@
 <script>
   export default {
     async asyncData({ params }) {
-      // const bookPromise = process.BROWSER_BUILD
-      //   ? import('~/_content/books/' + params.slug + '.json')
-      //   : Promise.resolve(
-      //       require('~/_content/books/' + params.slug + '.json')
-      //     );
-      let book = await import('~/_content/books/' + params.slug + '.json');
-      return book;
+      const bookPromise = process.BROWSER_BUILD
+        ? import('~/_content/books/' + params.slug + '.json')
+        : Promise.resolve(
+            require('~/_content/books/' + params.slug + '.json')
+          );
+      // let book = await import('~/_content/books/' + params.slug + '.json');
+      // return book;
     }
   };
 </script>
